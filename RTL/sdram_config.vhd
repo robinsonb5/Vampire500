313e54d99f880cb2d28f19defda8a38ca228b5fd
Alastair M. Robinson <robinsonb5@talk21.com>
2013-07-14 16:42:14 +0100

    Added missing sdram_config.vhd

---
+++ RTL/sdram_config.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package sdram_config is
+
+constant sdram_rows : integer := 13;
+constant sdram_cols : integer := 9;
+
+end package;
